69aecebd874b30f9146d9d258368ba89a6cb86c8
Alex <57597221+AlexGuyNichols@users.noreply.github.com>
2022-05-23 15:01:44 +0100

    Create app.js

---
+++ DogYears/app.js
@@ -0,0 +1,13 @@
+
+let myAge = 29;
+
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+let laterYears=myAge - 2;
+laterYears *= 4;
+
+myAgeInDogYears = (earlyYears+laterYears);
+myName = ('Alex'.toLowerCase())
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
